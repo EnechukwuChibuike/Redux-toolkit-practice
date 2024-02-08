315393f98ad630da2cdd1ad716c234f7fcc5077f
Enechukwu Chibuike <enechukwuchibuike0@gmail.com>
2024-02-08 14:02:26 +0100

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,7 @@
-import Counter from "./features/counter/Counter";
 import "./App.css";
 
 function App() {
-  return (
-    <main>
-      <Counter />
-    </main>
-  );
+  return <main></main>;
 }
 
 export default App;
